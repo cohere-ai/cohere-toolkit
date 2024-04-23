85c6d6eb6882714dd98b0e81f90011ff39cd40ee
Luísa Moura <luisa@cohere.com>
2024-04-23 10:47:03 -0400

    Config: rename tools (#10)
    * Config: rename tools
    
    * rename file tool

--- src/backend/config/tools.py
+++ src/backend/config/tools.py
@@ -17,12 +17,12 @@ Don't forget to add the implementation to this AVAILABLE_TOOLS dictionary!
 
 
 class ToolName(StrEnum):
-    Wiki_Retriever_LangChain = "Wiki Retriever - LangChain"
-    File_Upload_Langchain = "File Upload - Langchain"
-    File_Upload_LlamaIndex = "File Upload - LlamaIndex"
+    Wiki_Retriever_LangChain = "Wikipedia"
+    File_Upload_Langchain = "File Reader"
+    File_Upload_LlamaIndex = "File Reader - LlamaIndex"
     Python_Interpreter = "Python_Interpreter"
     Calculator = "Calculator"
-    Tavily_Internet_Search = "Internet Search - Tavily"
+    Tavily_Internet_Search = "Internet Search"
     Arxiv = "Arxiv"
     Pub_Med = "Pub Med"
 
