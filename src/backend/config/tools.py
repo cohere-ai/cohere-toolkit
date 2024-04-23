b16ac4841c021adee6111f45411e6acc823b99a3
lusmoura <luisa@cohere.com>
2024-04-23 15:44:01 +0100

    rename file tool

--- src/backend/config/tools.py
+++ src/backend/config/tools.py
@@ -18,8 +18,8 @@ Don't forget to add the implementation to this AVAILABLE_TOOLS dictionary!
 
 class ToolName(StrEnum):
     Wiki_Retriever_LangChain = "Wikipedia"
-    File_Upload_Langchain = "File Upload"
-    File_Upload_LlamaIndex = "File Upload - LlamaIndex"
+    File_Upload_Langchain = "File Reader"
+    File_Upload_LlamaIndex = "File Reader - LlamaIndex"
     Python_Interpreter = "Python_Interpreter"
     Calculator = "Calculator"
     Tavily_Internet_Search = "Internet Search"
