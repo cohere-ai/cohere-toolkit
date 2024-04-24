7936fbfdcb9fe73a5b30f30b531a16efa949fc26
KevinLuong96 <kevin@cohere.com>
2024-04-24 14:29:50 -0400

    Langchain Multihop as an experimental feature (#35)
    * add langchain storage
    
    * feat: experimental langchain endpoints and update readme
    
    * rebase
    
    * format
    
    * reformat
    
    * docstring fixes and update langchain chat request object
    
    * update readme and address comment

--- src/backend/chat/custom/langchain.py
+++ src/backend/chat/custom/langchain.py
@@ -6,48 +6,15 @@ from langchain_cohere.react_multi_hop.agent import create_cohere_react_agent
 from langchain_core.prompts import ChatPromptTemplate
 
 from backend.chat.base import BaseChat
-from backend.config.tools import AVAILABLE_TOOLS, ToolName
+from backend.config.tools import AVAILABLE_TOOLS
 from backend.schemas.langchain_chat import LangchainChatRequest
 
 
 class LangChainChat(BaseChat):
     ### WARNING: This is an experimental feature using Langchain. We encourage you to use tool use through the Cohere API!
-    """Custom chat flow using Langchain"""
+    ### Please see Langchain Multihop documentation in the Readme
 
-    def __init__(self) -> None:
-        llm = ChatCohere(model="command-r-plus")
-
-        self.default_preamble = """
-        You are an expert who answers the user's question with the most relevant datasource. You are equipped with an internet search tool and a special vectorstore of information about how to write good essays.
-        You also have a 'random_operation_tool' tool, you must use it to compute the random operation between two numbers.
-        """
-
-        # Prompt template
-        prompt = ChatPromptTemplate.from_template("{input}")
-
-        python_tool = AVAILABLE_TOOLS[ToolName.Python_Interpreter].implementation()
-        tavily_search_tool = AVAILABLE_TOOLS[
-            ToolName.Tavily_Internet_Search
-        ].implementation()
-        # Create the ReAct agent
-        agent = create_cohere_react_agent(
-            llm=llm,
-            # TODO get tools from chat req
-            tools=[
-                tavily_search_tool.to_langchain_tool(),
-                python_tool.to_langchain_tool(),
-            ],
-            prompt=prompt,
-        )
-
-        self.agent_executor = AgentExecutor(
-            agent=agent,
-            tools=[
-                tavily_search_tool.to_langchain_tool(),
-                python_tool.to_langchain_tool(),
-            ],
-            verbose=True,
-        )
+    """Custom experimental chat flow using Langchain. Multihop tool usage is enabled"""
 
     def chat(self, chat_request: LangchainChatRequest, **kwargs: Any) -> Any:
         """
@@ -60,7 +27,37 @@ class LangChainChat(BaseChat):
         Returns:
             Generator[StreamResponse, None, None]: Chat response.
         """
-        # TODO: handle chat history and persistence
+
+        llm = ChatCohere(model="command-r-plus")
+
+        self.default_preamble = """
+        You are an expert who answers the user's question with the most relevant datasource. You are equipped with an internet search tool and a special vectorstore of information about how to write good essays.
+        You also have a 'random_operation_tool' tool, you must use it to compute the random operation between two numbers.
+        """
+
+        # Prompt template
+        prompt = ChatPromptTemplate.from_template("{input}")
+
+        tools = []
+        for req_tool in chat_request.tools:
+            tool = AVAILABLE_TOOLS.get(req_tool.name)
+            if tool:
+                tools.append(tool.implementation().to_langchain_tool())
+            else:
+                raise ValueError(f"Tool {req_tool.name} not found")
+
+        # Create the ReAct agent
+        agent = create_cohere_react_agent(
+            llm=llm,
+            tools=tools,
+            prompt=prompt,
+        )
+
+        self.agent_executor = AgentExecutor(
+            agent=agent,
+            tools=tools,
+            verbose=True,
+        )
 
         return self.agent_executor.stream(
             {
--- src/backend/schemas/chat.py
+++ src/backend/schemas/chat.py
@@ -8,7 +8,7 @@ from backend.chat.enums import StreamEvent
 from backend.schemas.citation import Citation
 from backend.schemas.document import Document
 from backend.schemas.search_query import SearchQuery
-from backend.schemas.tool import ToolCall
+from backend.schemas.tool import Tool, ToolCall
 
 
 class ChatRole(StrEnum):
@@ -135,11 +135,11 @@ class StreamSearchQueriesGeneration(ChatResponse):
 
 
 class StreamEnd(ChatResponse):
-    response_id: str
+    response_id: str | None
     event_type: ClassVar[StreamEvent] = StreamEvent.STREAM_END
     is_finished: ClassVar[bool] = True
-    generation_id: str = Field()
-    conversation_id: Optional[str] = Field()
+    generation_id: str | None = Field()
+    conversation_id: str | None = Field()
     text: str = Field(
         title="Contents of the chat message.",
     )
@@ -238,4 +238,59 @@ class BaseChatRequest(BaseModel):
     conversation_id: str = Field(
         default_factory=lambda: str(uuid4()),
         title="To store a conversation then create a conversation id and use it for every related request",
+    )
+
+    tools: List[Tool] | None = Field(
+        default_factory=list,
+        title="""
+            List of custom or managed tools to use for the response.
+            If passing in managed tools, you only need to provide the name of the tool.
+            If passing in custom tools, you need to provide the name, description, and optionally parameter defintions of the tool.
+            Passing a mix of custom and managed tools is not supported. 
+
+            Managed Tools Examples:
+            tools=[
+                {
+                    "name": "Wiki Retriever - LangChain",
+                },
+                {
+                    "name": "Calculator",
+                }
+            ]
+
+            Custom Tools Examples:
+            tools=[
+                {
+                    "name": "movie_title_generator",
+                    "description": "tool to generate a cool movie title",
+                    "parameter_definitions": {
+                        "synopsis": {
+                            "description": "short synopsis of the movie",
+                            "type": "str",
+                            "required": true
+                        }
+                    }
+                },
+                {
+                    "name": "random_number_generator",
+                    "description": "tool to generate a random number between min and max",
+                    "parameter_definitions": {
+                        "min": {
+                            "description": "minimum number",
+                            "type": "int",
+                            "required": true
+                        },
+                        "max": {
+                            "description": "maximum number",
+                            "type": "int",
+                            "required": true
+                        }
+                    }  
+                },
+                {
+                    "name": "joke_generator",
+                    "description": "tool to generate a random joke",
+                }
+            ]
+        """,
     )
