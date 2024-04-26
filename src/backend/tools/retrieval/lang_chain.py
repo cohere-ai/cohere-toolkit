428b9f2651f9972712428518d87c1cbcafcdc017
Tianjing Li <tianjinglimail@gmail.com>
2024-04-26 13:16:54 -0400

    Fix: File Upload not generating message (#55)
    * Fix file upload
    
    * lint

--- src/backend/tools/retrieval/lang_chain.py
+++ src/backend/tools/retrieval/lang_chain.py
@@ -2,8 +2,8 @@ import os
 from typing import Any, Dict, List
 
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_cohere import CohereEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.embeddings import CohereEmbeddings
 from langchain_community.retrievers import WikipediaRetriever
 from langchain_community.vectorstores import Chroma
 
@@ -20,8 +20,7 @@ More details: https://python.langchain.com/docs/integrations/retrievers
 class LangChainWikiRetriever(BaseRetrieval):
     """
     This class retrieves documents from Wikipedia using the langchain package.
-    This requires wikipedia package to be installed:
-    pip install wikipedia-api
+    This requires wikipedia package to be installed.
     """
 
     def __init__(self, chunk_size: int = 300, chunk_overlap: int = 0):
@@ -48,22 +47,19 @@ class LangChainWikiRetriever(BaseRetrieval):
 class LangChainVectorDBRetriever(BaseRetrieval):
     """
     This class retrieves documents from a vector database using the langchain package.
-    This requires chromadb package to be installed:
-     pip install chromadb
     """
 
     def __init__(self, filepath: str):
+        self.cohere_api_key = os.environ.get("COHERE_API_KEY")
         self.filepath = filepath
 
     def retrieve_documents(self, query: str, **kwargs: Any) -> List[Dict[str, Any]]:
-        cohere_embeddings = CohereEmbeddings(
-            cohere_api_key=os.environ["COHERE_API_KEY"]
-        )
+        cohere_embeddings = CohereEmbeddings(cohere_api_key=self.cohere_api_key)
         # Load text files and split into chunks
         loader = PyPDFLoader(self.filepath)
         text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0)
         pages = loader.load_and_split(text_splitter)
         # Create a vector store from the documents
-        db = Chroma.from_documents(pages, cohere_embeddings)
+        db = Chroma.from_documents(documents=pages, embedding=cohere_embeddings)
         input_docs = db.as_retriever().get_relevant_documents(query)
         return [dict({"text": doc.page_content}) for doc in input_docs]
