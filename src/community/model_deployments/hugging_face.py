2a53f6ae8aa3e5bc61b341db105377bd7a931d29
lusmoura <luisa@cohere.com>
2024-04-29 16:02:03 +0100

    fix hugging face

--- src/community/model_deployments/hugging_face.py
+++ src/community/model_deployments/hugging_face.py
@@ -3,10 +3,16 @@ from typing import List, Dict, Generator, Any
 from community.model_deployments import BaseDeployment
 from backend.schemas.cohere_chat import CohereChatRequest
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from cohere.types import StreamedChatResponse
+from cohere.types import StreamedChatResponse, NonStreamedChatResponse
 
 
 class HuggingFaceDeployment(BaseDeployment):
+    """
+    The first time you run this code, it will download all the shards of the model from the Hugging Face model hub.
+    This usually takes a while, so you might want to run this code separately and not as part of the toolkit.
+    For that, you can run the following command:
+        poetry run python3 src/community/model_deployments/hugging_face.py
+    """
     DEFAULT_MODELS = ["CohereForAI/c4ai-command-r-v01", "CohereForAI/c4ai-command-r-plus"]
     def __init__(self):
         pass
@@ -31,18 +37,13 @@ class HuggingFaceDeployment(BaseDeployment):
         if model_id == "command-r":
             model_id = self.DEFAULT_MODELS[0]
 
-        print(model_id)
-
         tokenizer = AutoTokenizer.from_pretrained(model_id)
-        print(tokenizer)
         model = AutoModelForCausalLM.from_pretrained(model_id)
         
-        print(model)
         # Format message with the command-r-plus chat template
         messages = self._build_chat_history(chat_request.chat_history, chat_request.message)
-        print(messages)
         input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
-        print(input_ids)
+
         gen_tokens = model.generate(
             input_ids, 
             max_new_tokens=100, 
@@ -50,42 +51,26 @@ class HuggingFaceDeployment(BaseDeployment):
             temperature=0.3,
             )
 
-        print(gen_tokens)
-
         gen_text = tokenizer.decode(gen_tokens[0])
         
-        print(gen_text)
-        exit()
-        return gen_text
+        return {"text": gen_text}
     
 
     def invoke_chat_stream(
         self, chat_request: CohereChatRequest, **kwargs: Any
     ) -> Generator[StreamedChatResponse, None, None]:
-        model_id = chat_request.model
-        if model_id is None:
-            model_id = self.DEFAULT_MODELS[0]
-
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(model_id)
-
-        # Format message with the command-r-plus chat template
+        """
+        Built in streamming is not supported, so this function wraps the invoke_chat function to return a single response.
+        """
         messages = self._build_chat_history(chat_request.chat_history, chat_request.message)
-        input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+        gen_text = self.invoke_chat(chat_request)
 
-        gen_tokens = model.generate_stream(
-            input_ids, 
-            max_new_tokens=100, 
-            do_sample=True, 
-            temperature=0.3,
-            )
-        
-        print(gen_tokens)
-        
-        gen_text = tokenizer.decode(gen_tokens[0])
         yield StreamedChatResponse(
-            message=gen_text,
-            chat_history=messages
+            finish_reason="COMPLETE",
+            response=NonStreamedChatResponse(
+                text=gen_text.get("text", ""),
+                chat_history=messages
+            )
         )
 
 
@@ -126,4 +111,3 @@ if __name__ == "__main__":
         message="How are you?"
     )
     response = hugging_face.invoke_chat(chat_request)
-    print(response)
