1145eed44cee17da29cefc3cf3609eefc6e078a5
Luísa Moura <luisa@cohere.com>
2024-04-29 12:14:15 -0400

    community: add community folder (#53)
    * Community: add community folder
    
    * hugging face + update docs
    
    * fix hugging face
    
    * lint

---
+++ src/community/model_deployments/hugging_face.py
@@ -0,0 +1,125 @@
+import logging
+from typing import Any, Dict, Generator, List
+
+from cohere.types import NonStreamedChatResponse, StreamedChatResponse
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from backend.schemas.cohere_chat import CohereChatRequest
+from community.model_deployments import BaseDeployment
+
+
+class HuggingFaceDeployment(BaseDeployment):
+    """
+    The first time you run this code, it will download all the shards of the model from the Hugging Face model hub.
+    This usually takes a while, so you might want to run this code separately and not as part of the toolkit.
+    For that, you can run the following command:
+        poetry run python3 src/community/model_deployments/hugging_face.py
+    """
+
+    DEFAULT_MODELS = [
+        "CohereForAI/c4ai-command-r-v01",
+        "CohereForAI/c4ai-command-r-plus",
+    ]
+
+    def __init__(self):
+        pass
+
+    @property
+    def rerank_enabled(self) -> bool:
+        return False
+
+    @classmethod
+    def list_models(cls) -> List[str]:
+        if not HuggingFaceDeployment.is_available():
+            return []
+
+        return cls.DEFAULT_MODELS
+
+    @classmethod
+    def is_available(cls) -> bool:
+        return True
+
+    def invoke_chat(self, chat_request: CohereChatRequest, **kwargs: Any) -> Any:
+        model_id = chat_request.model
+        if model_id == "command-r":
+            model_id = self.DEFAULT_MODELS[0]
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+
+        # Format message with the command-r-plus chat template
+        messages = self._build_chat_history(
+            chat_request.chat_history, chat_request.message
+        )
+        input_ids = tokenizer.apply_chat_template(
+            messages, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+        )
+
+        gen_tokens = model.generate(
+            input_ids,
+            max_new_tokens=100,
+            do_sample=True,
+            temperature=0.3,
+        )
+
+        gen_text = tokenizer.decode(gen_tokens[0])
+
+        return {"text": gen_text}
+
+    def invoke_chat_stream(
+        self, chat_request: CohereChatRequest, **kwargs: Any
+    ) -> Generator[StreamedChatResponse, None, None]:
+        """
+        Built in streamming is not supported, so this function wraps the invoke_chat function to return a single response.
+        """
+        messages = self._build_chat_history(
+            chat_request.chat_history, chat_request.message
+        )
+        gen_text = self.invoke_chat(chat_request)
+
+        yield StreamedChatResponse(
+            finish_reason="COMPLETE",
+            response=NonStreamedChatResponse(
+                text=gen_text.get("text", ""), chat_history=messages
+            ),
+        )
+
+    def invoke_search_queries(
+        self,
+        message: str,
+        chat_history: List[Dict[str, str]] | None = None,
+        **kwargs: Any,
+    ) -> list[str]:
+        logging.warning(
+            "invoke_search_queries not implemented for HuggingFaceDeployment"
+        )
+        return []
+
+    def invoke_rerank(
+        self, query: str, documents: List[Dict[str, Any]], **kwargs: Any
+    ) -> Any:
+        return None
+
+    def _build_chat_history(
+        self, chat_history: List[Dict[str, Any]], message: str
+    ) -> List[Dict[str, Any]]:
+        messages = []
+
+        for message in chat_history:
+            messages.append({"role": message["role"], "content": message["message"]})
+
+        messages.append({"role": "USER", "content": message})
+
+        return messages
+
+
+if __name__ == "__main__":
+    hugging_face = HuggingFaceDeployment()
+    chat_request = CohereChatRequest(
+        chat_history=[
+            {"role": "USER", "message": "Hello!"},
+            {"role": "CHATBOT", "message": "Hi, how can I help you?"},
+        ],
+        message="How are you?",
+    )
+    response = hugging_face.invoke_chat(chat_request)
