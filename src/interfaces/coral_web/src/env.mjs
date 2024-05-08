0d15ece9c7919c67c5ff28e966406e6306851f8b
Lakshya Agarwal <lakshyajannu@gmail.com>
2024-05-08 15:16:06 -0400

    feat: update env variables and templates

--- src/interfaces/coral_web/src/env.mjs
+++ src/interfaces/coral_web/src/env.mjs
@@ -7,9 +7,11 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_API_HOSTNAME: z.string(),
+    NEXT_PUBLIC_HAS_CUSTOM_LOGO: z.string().optional().default('false'),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_HOSTNAME: process.env.NEXT_PUBLIC_API_HOSTNAME,
+    NEXT_PUBLIC_HAS_CUSTOM_LOGO: process.env.NEXT_PUBLIC_HAS_CUSTOM_LOGO,
   },
   emptyStringAsUndefined: true,
   skipValidation: ['lint', 'format', 'test', 'test:coverage', 'test:watch'].includes(
