8055eddf4b9420c9ddcc2e34303d9665baf7cd38
EugeneP <eugene@lightsonsoftware.com>
2024-05-30 17:30:36 +0200

    GCP one click deployment postcreate hook

--- gcp_postcreate_hook.sh
+++ gcp_postcreate_hook.sh
@@ -4,5 +4,13 @@ echo "Project $GOOGLE_CLOUD_PROJECT"
 echo "Service $K_SERVICE"
 echo "URL $SERVICE_URL"
 gcloud run services describe toolkit-deploy --project oss-toolkit --region us-central1 --format export > service.yaml
-sed -i '/startupProbe:/,/timeoutSeconds: 240/c\    startupProbe:\      timeoutSeconds: 1\      periodSeconds: 1\      failureThreshold: 120\      httpGet:\        path: /api/health\        port: 4000' service.yaml
-#gcloud run services replace service.yaml --project oss-toolkit --region us-central1
+sed -i '/template:/,/spec:/s/^\(\s*name:\).*/\1 ""/' service.yaml
+sed -i '/startupProbe:/,/timeoutSeconds: 240/c\
+        startupProbe:\
+          timeoutSeconds: 1\
+          periodSeconds: 1\
+          failureThreshold: 120\
+          httpGet:\
+            path: /api/health\
+            port: 4000' service.yaml
+gcloud run services replace service.yaml --project oss-toolkit --region us-central1
