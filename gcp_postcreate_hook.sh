614a404bac35d69f97bb8336dbf4c757543d3628
EugeneP <eugene@lightsonsoftware.com>
2024-05-30 18:02:33 +0200

    GCP one click deployment postcreate hook - final

--- gcp_postcreate_hook.sh
+++ gcp_postcreate_hook.sh
@@ -3,7 +3,7 @@ echo "Region $GOOGLE_CLOUD_REGION"
 echo "Project $GOOGLE_CLOUD_PROJECT"
 echo "Service $K_SERVICE"
 echo "URL $SERVICE_URL"
-gcloud run services describe toolkit-deploy --project oss-toolkit --region us-central1 --format export > service.yaml
+gcloud run services describe toolkit-deploy --project $GOOGLE_CLOUD_PROJECT --region $GOOGLE_CLOUD_REGION --format export > service.yaml
 sed -i '/template:/,/spec:/s/^\(\s*name:\).*/\1 ""/' service.yaml
 sed -i '/startupProbe:/,/timeoutSeconds: 240/c\
         startupProbe:\
@@ -13,4 +13,4 @@ sed -i '/startupProbe:/,/timeoutSeconds: 240/c\
           httpGet:\
             path: /api/health\
             port: 4000' service.yaml
-gcloud run services replace service.yaml --project oss-toolkit --region us-central1
+gcloud run services replace service.yaml --project $GOOGLE_CLOUD_PROJECT --region $GOOGLE_CLOUD_REGION
