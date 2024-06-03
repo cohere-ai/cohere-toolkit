abe29dc902212f51c5e82de7f92daedf2b5ba62a
Tomeu Cabot <tomeu@cohere.com>
2024-06-03 13:38:04 +0200

    refactor: clean code

--- src/interfaces/coral_web/src/components/MessageContent.tsx
+++ src/interfaces/coral_web/src/components/MessageContent.tsx
@@ -17,6 +17,7 @@ import {
   isLoadingMessage,
 } from '@/types/message';
 import { cn } from '@/utils';
+import { replaceCodeBlockWithIframe } from '@/utils/preview';
 
 type Props = {
   isLast: boolean;
@@ -25,97 +26,6 @@ type Props = {
 };
 
 const BOT_ERROR_MESSAGE = 'Unable to generate a response since an error was encountered. ';
-
-function addOnErrorToImg(html: string) {
-  //add an onError=getImage(this) to all img elements in the html string
-  //this is a hacky way to do it but it works
-  const regex = /<img([\s\S]+?)>/g;
-  const imgElements = html.match(regex);
-  if (imgElements) {
-    for (let i = 0; i < imgElements.length; i++) {
-      const imgElement = imgElements[i];
-      const newImgElement = imgElement.replace('>', ' onError="getImage(this)">');
-      html = html.replace(imgElement, newImgElement);
-    }
-  }
-  html = html + GET_IMAGE_DEF;
-  return html;
-}
-const GET_IMAGE_DEF = `
-<script>
-function getImage(element) {
-  console.log(element.alt);
-  console.log(element);
-
-  return fetch(
-    'https://api.pexels.com/v1/search?query=' + element.alt + '&per_page=1&total_results=1',
-    {
-      method: 'GET',
-      headers: {
-        Authorization: 'JMr4ZI1Ap8UmPwuPbFBjjNzvnzD3urK7l8EQTpHa66PdDhOOfebAe426',
-      },
-      // We are not sending any data in the request, so we use null as the body.
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      element.src = data['photos'][0]['src']['original'];
-      this.src = data['photos'][0]['src']['original'];
-    });
-}
-</script>
-`;
-function getReconstructedHtml(plainString: string) {
-  const htmlRegex = /```(?:html)\n([\s\S]*?)(```|$)/;
-  //capture only the code in the markdown block exlcuding the html tag
-  const code = plainString.match(htmlRegex);
-  const plainText = plainString.replace(htmlRegex, '');
-  var html = '';
-  if (code) {
-    html = code[1];
-    console.log(html);
-
-    const cssRegex = /```(?:css)\n([\s\S]*?)```/;
-    const cssCode = plainString.match(cssRegex);
-    //const plainCSS = plainString.replace(cssRegex, '');
-    var css = '';
-    if (cssCode) {
-      css = cssCode[1];
-      html = `<style>${css}</style>` + html;
-    }
-
-    const jsRegex = /```(?:js)\n([\s\S]*?)```/;
-    const jsCode = plainString.match(jsRegex);
-    //const plainCSS = plainString.replace(jsRegex, '');
-    var js = '';
-    if (jsCode) {
-      js = jsCode[1];
-      html = html + `<script>${js}</script>`;
-    }
-    html = html + GET_IMAGE_DEF;
-  }
-  return html;
-}
-const replaceCodeBlockWithIframe = (content: string) => {
-  const matchingRegex = /```html([\s\S]+)/;
-  const replacingRegex = /```html([\s\S]+?)(```|$)/;
-
-  const match = content.match(matchingRegex);
-
-  if (!match) {
-    return content;
-  }
-
-  const html = addOnErrorToImg(getReconstructedHtml(content));
-
-  const blob = new Blob([html], { type: 'text/html' });
-  const src = URL.createObjectURL(blob);
-  const iframe = `<iframe data-src="${src}"></iframe>`;
-
-  content = content.replace(replacingRegex, iframe);
-
-  return content;
-};
 
 export const MessageContent: React.FC<Props> = ({ isLast, message, onRetry }) => {
   const isUser = message.type === MessageType.USER;
@@ -192,8 +102,11 @@ export const MessageContent: React.FC<Props> = ({ isLast, message, onRetry }) =>
   } else {
     const hasCitations =
       isTypingOrFulfilledMessage && message.citations && message.citations.length > 0;
-    // replace the code block with an iframe
-    const md = replaceCodeBlockWithIframe(message.originalText);
+    let md = message.text;
+    if (isFulfilledMessage(message)) {
+      // replace the code block with an iframe
+      md = replaceCodeBlockWithIframe(message.originalText);
+    }
     content = (
       <>
         <Markdown
