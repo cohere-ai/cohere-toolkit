66e8e2779fa81e3b56f222b6c435b96be985072e
Scott <scottchu@cohere.com>
2024-06-05 09:42:40 -0400

    changes

---
+++ src/backend/database_models/agent.py
@@ -0,0 +1,36 @@
+from typing import List
+from enum import StrEnum
+
+from sqlalchemy import UniqueConstraint, Integer, String, Float, SQLEnum, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.database_models.base import Base
+
+class Deployment(StrEnum):
+    COHERE_PLATFORM = "Cohere Platform"
+    SAGE_MAKER = "SageMaker"
+    AZURE = "Azure"
+    BEDROCK = "Bedrock"
+
+class Model(StrEnum):
+    COMMAND_R = "command-r"
+    COMMAND_R_PLUS = "command-r-plus"
+    COMMAND_LIGHT = "command-light"
+    COMMAND = "command"
+
+class Agent(Base):
+    __tablename__ = "agents"
+
+    version: Mapped[int] = mapped_column(Integer, default=1)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    preamble: Mapped[str] = mapped_column(String, nullable=True)
+    temperature: Mapped[float] = mapped_column(Float, default=0.3)
+    # tool: Mapped[List["Tool"]] = relationship()
+
+    model: Mapped[Model] = mapped_column(SQLEnum(Model), nullable=False)
+    deployment: Mapped[Deployment] = mapped_column(SQLEnum(Deployment), nullable=False)
+
+    # org_id: Mapped[str] = mapped_column(String, nullable=False)
+
+    __table_args__ = (UniqueConstraint('org_id', 'name', name='_org_id_name_uc'),)
