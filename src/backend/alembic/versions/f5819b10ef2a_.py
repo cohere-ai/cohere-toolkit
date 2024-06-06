56cd8e65891bc93a1a38e67b1b3fe4ee61e4fda5
lusmoura <luisa@cohere.com>
2024-06-06 17:16:04 +0100

    fix migration

--- src/backend/alembic/versions/f5819b10ef2a_.py
+++ src/backend/alembic/versions/f5819b10ef2a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 78b159bee0a6
-Revises: c15b848babe3
-Create Date: 2024-06-03 12:46:37.439991
+Revision ID: f5819b10ef2a
+Revises: 3247f8fd3f71
+Create Date: 2024-06-06 16:13:32.066454
 
 """
 
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "78b159bee0a6"
-down_revision: Union[str, None] = "c15b848babe3"
+revision: str = "f5819b10ef2a"
+down_revision: Union[str, None] = "3247f8fd3f71"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
