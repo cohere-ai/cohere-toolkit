1f40d3b19ca1f67a43e8192815a032a88a1e53cd
Scott <scottchu@cohere.com>
2024-06-10 12:23:26 -0400

    update

--- src/interfaces/coral_web/src/cohere-client/generated/models/DeleteAgent.ts
+++ src/interfaces/coral_web/src/cohere-client/generated/models/DeleteAgent.ts
@@ -2,9 +2,4 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-/**
- * Stream text generation event.
- */
-export type StreamTextGeneration = {
-  text: string;
-};
+export type DeleteAgent = {};
--- src/interfaces/coral_web/src/cohere-client/generated/models/StreamSearchResults.ts
+++ src/interfaces/coral_web/src/cohere-client/generated/models/StreamSearchResults.ts
@@ -8,7 +8,6 @@
 import type { Document } from './Document';
 
 export type StreamSearchResults = {
-  is_finished: boolean;
   search_results?: Array<Record<string, any>>;
   documents?: Array<Document>;
 };
