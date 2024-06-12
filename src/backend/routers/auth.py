595b29698599dbb208aa59ec25f30a80ef410fb6
Tianjing Li <tianjinglimail@gmail.com>
2024-06-12 17:53:14 -0400

    lint

--- src/backend/routers/auth.py
+++ src/backend/routers/auth.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from authlib.integrations.starlette_client import OAuthError
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from starlette.requests import Request
 
 from backend.config.auth import ENABLED_AUTH_STRATEGY_MAPPING
