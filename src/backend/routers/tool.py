9d4cdd136283fd55206ff5a7abab3751500e15b7
Scott <scottchu@cohere.com>
2024-06-12 15:47:27 -0400

    filter tools by agent

--- src/backend/routers/tool.py
+++ src/backend/routers/tool.py
@@ -1,19 +1,35 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException
+from typing import Optional
 from backend.config.routers import RouterName
 from backend.config.tools import AVAILABLE_TOOLS
 from backend.schemas.tool import ManagedTool
+from backend.crud import agent as agent_crud
+from backend.database_models.database import DBSessionDep
 
 router = APIRouter(prefix="/v1/tools")
 router.name = RouterName.TOOL
 
 
 @router.get("", response_model=list[ManagedTool])
-def list_tools() -> list[ManagedTool]:
+def list_tools(session: DBSessionDep, agent_id: str | None = None) -> list[ManagedTool]:
     """
     List all available tools.
 
     Returns:
         list[ManagedTool]: List of available tools.
     """
+    if agent_id:
+        agent_tools = []
+        agent = agent_crud.get_agent(session, agent_id)
+
+        if not agent:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Agent with ID: {agent_id} not found.",
+            )
+
+        for tool in agent.tools:
+            agent_tools.append(AVAILABLE_TOOLS[tool])
+        return agent_tools
+
     return AVAILABLE_TOOLS.values()
