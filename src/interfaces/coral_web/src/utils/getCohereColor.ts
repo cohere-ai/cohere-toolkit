833bd3d153570583d04e36e71154d5059abc677b
Khalil Najjar <knajjars@gmail.com>
2024-06-17 18:09:08 +0200

    improve animation for agents side panel

--- src/interfaces/coral_web/src/utils/getCohereColor.ts
+++ src/interfaces/coral_web/src/utils/getCohereColor.ts
@@ -18,7 +18,7 @@ export const getCohereColor = (id?: string) => {
 
     return COLOR_LIST[randomIndex];
   }
-
-  const index = id.charCodeAt(0) % COLOR_LIST.length;
+  const idNumber = id.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  const index = idNumber % COLOR_LIST.length;
   return COLOR_LIST[index];
 };
