6b304715d0698db0bc7b5b94847dbba64359041d
Scott <scottchu@cohere.com>
2024-06-19 15:06:24 -0400

    default model for agents right now

--- src/backend/services/chat.py
+++ src/backend/services/chat.py
@@ -85,7 +85,9 @@ def process_chat(
         # Set the agent settings in the chat request
         chat_request.preamble = agent.preamble
         chat_request.tools = [Tool(name=tool) for tool in agent.tools]
-        chat_request.model = agent.model
+        # NOTE TEMPORARY: we do not set a the model for now and just use the default model
+        chat_request.model = None
+        # chat_request.model = agent.model
 
     should_store = chat_request.chat_history is None and not is_custom_tool_call(
         chat_request
