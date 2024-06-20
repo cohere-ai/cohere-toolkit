896a45cc4edb089a49cf7bc534bfbb051e81d8d2
Scott <scottchu@cohere.com>
2024-06-20 12:06:59 -0400

    fix validators

--- src/backend/services/request_validators.py
+++ src/backend/services/request_validators.py
@@ -22,7 +22,7 @@ def validate_user_header(request: Request):
 
     """
 
-    user_id = get_header_user_id(request)
+    user_id = request.headers.get(request)
     if not user_id:
         raise HTTPException(
             status_code=401, detail="User-Id required in request headers."
