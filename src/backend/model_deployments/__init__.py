e291e5369c01722d26f13a5c844436bcc3d842c5
Miguel Varela Ramos <miguel@cohere.ai>
2024-06-24 12:15:06 +0100

    feat: single container deployment

--- src/backend/model_deployments/__init__.py
+++ src/backend/model_deployments/__init__.py
@@ -1,11 +1,13 @@
 from backend.model_deployments.azure import AzureDeployment
 from backend.model_deployments.bedrock import BedrockDeployment
 from backend.model_deployments.cohere_platform import CohereDeployment
+from backend.model_deployments.single_container import SingleContainerDeployment
 from backend.model_deployments.sagemaker import SageMakerDeployment
 
 __all__ = [
     "AzureDeployment",
     "CohereDeployment",
+    "SingleContainerDeployment",
     "SageMakerDeployment",
     "BedrockDeployment",
 ]
