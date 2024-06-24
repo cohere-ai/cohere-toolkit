d8044bc8dc4dcb83fd570aa97e1c30a9896d4195
Khalil Najjar <knajjars@gmail.com>
2024-06-24 17:53:23 +0200

    code feedback

--- src/interfaces/coral_web/src/utils/getSlugRoutes.ts
+++ src/interfaces/coral_web/src/utils/getSlugRoutes.ts
@@ -1,35 +1,38 @@
-/**
- *
- * @description This funciton is used to parse the slug from the URL and return the agentId and conversationId.
- * The slug can be in the following formats:
- * - [] - /
- * - [c, :conversationId] - /c/:conversationId
- * - [:agentId] - /:agentId
- * - [:agentId, c, :conversationId] - /:agentId/c/:conversationId
- */
-
 const isUUID = (value: string) => {
   const isValidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
   return isValidRegex.test(value);
 };
 
+/**
+ *
+ * @description This function is used to parse the slug from the URL and return the agentId and conversationId.
+ * The slug can be in the following formats:
+ * - [] - /
+ * - [c, :conversationId] - /c/:conversationId
+ * - [:agentId] - /:agentId
+ * - [:agentId, c, :conversationId] - /:agentId/c/:conversationId
+ */
 export const getSlugRoutes = (
   slugParams?: string | string[]
 ): {
   agentId: string | undefined;
   conversationId: string | undefined;
 } => {
-  if (!slugParams || typeof slugParams === 'string')
+  if (!slugParams || typeof slugParams === 'string') {
     return { agentId: undefined, conversationId: undefined };
+  }
 
+  // Possible values are:
   // firstQuery = :agentId | c | undefined
   // secondQuery = c | :conversationId | undefined
   // thirdQuery = :conversationId | undefined
   const [firstQuery, secondQuery, thirdQuery] = slugParams;
 
   // []
-  if (!firstQuery) return { agentId: undefined, conversationId: undefined };
+  if (!firstQuery) {
+    return { agentId: undefined, conversationId: undefined };
+  }
 
   // [c, :conversationId]
   if (firstQuery === 'c' && isUUID(secondQuery)) {
