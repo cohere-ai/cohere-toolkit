20b837fce366c60926267f06cd014c588158bcfa
Tomeu Cabot <tomeu@cohere.com>
2024-06-26 16:26:43 +0200

    better implementation of removeExtraBlankSpaces

--- src/interfaces/coral_web/src/components/Shared/Markdown/directives/utils.ts
+++ src/interfaces/coral_web/src/components/Shared/Markdown/directives/utils.ts
@@ -1,14 +1,19 @@
 import { Root } from 'hast';
 import type { Plugin } from 'unified';
-import { visit } from 'unist-util-visit';
 
 /**
- * A remark plugin that removes extra blank spaces from the text.
+ * A rehype plugin that removes extra blank spaces between tables.
  */
 export const removeExtraBlankSpaces: Plugin<void[], Root> = () => {
   return (tree) => {
-    visit(tree, 'text', (node) => {
-      node.value = node.value.replace(/\n{2,}/g, '\n');
-    });
+    for (let i = 0; i < tree.children.length; i++) {
+      const node = tree.children[i];
+      if (node.type == 'element' && node.tagName == 'table' && i > 0) {
+        const prevNode = tree.children[i - 1];
+        if (prevNode.type == 'text') {
+          prevNode.value = prevNode.value.replace(/\n{2,}/g, '\n');
+        }
+      }
+    }
   };
 };
