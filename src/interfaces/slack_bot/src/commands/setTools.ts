71734c5c5199b21b18e1cb592ac51e0f42f14810
Scott M <scottmx81@users.noreply.github.com>
2024-06-26 11:17:45 -0600

    slack-bot: Add new Slack bot interface to Cohere Toolkit (#177)
    Add Slack bot interface to toolkit

---
+++ src/interfaces/slack_bot/src/commands/setTools.ts
@@ -0,0 +1,170 @@
+import { KnownBlock, Middleware, MrkdwnOption, SlackCommandMiddlewareArgs } from '@slack/bolt';
+
+import { ApiError, OpenAPI, ToolkitClient } from '../cohere-client';
+import { ERRORS } from '../constants';
+import { handleError } from '../handlers';
+import { getChannelSettings } from '../utils/getChannelSettings';
+
+// Tools list command, brings up a modal for user to select tools
+export const setTools: Middleware<SlackCommandMiddlewareArgs> = async ({
+  client,
+  ack,
+  respond,
+  command,
+  context,
+  body,
+}) => {
+  await ack();
+
+  const channelId = command.channel_id;
+  const basicErrorHandlerArgs = {
+    client,
+    userId: command.user_id,
+    replyChannelId: command.channel_id,
+  };
+
+  const { tools: preEnabledToolsList } = await getChannelSettings({
+    teamId: context.teamId,
+    enterpriseId: context.enterpriseId,
+    channelId,
+  });
+
+  const getToolsListFromAPI = async () => {
+    try {
+      const toolkitClient = new ToolkitClient(OpenAPI);
+      const tools = await toolkitClient.default.listToolsToolsGet();
+
+      return tools.filter((t) => t.is_available).map((tool) => tool.name);
+    } catch (error: any) {
+      console.error(error);
+      let customErrorMsg = ERRORS.GENERAL;
+      if (error instanceof ApiError) {
+        customErrorMsg = error.message;
+      }
+      await handleError({
+        customErrorMsg: `${ERRORS.PREFIX} ${customErrorMsg}`,
+        ...basicErrorHandlerArgs,
+      });
+      return;
+    }
+  };
+
+  const apiToolsList = await getToolsListFromAPI();
+
+  if (!apiToolsList) {
+    await respond({
+      response_type: 'ephemeral',
+      text: `No tools are available in your account.`,
+    });
+    return;
+  }
+
+  const preEnabledToolsSet = new Set(preEnabledToolsList.map((t) => t.name));
+  const formattedPreEnabledToolsList = apiToolsList.reduce<MrkdwnOption[]>((acc, tool) => {
+    if (preEnabledToolsSet.has(tool)) {
+      acc.push({
+        text: { type: 'mrkdwn', text: tool },
+        value: tool,
+      });
+    }
+    return acc;
+  }, []);
+
+  // Remove pre-enabled tools from the list of available tools so we can
+  // Display those in a diff checkbox list
+  const formattedAndSanitizedApiToolsList = apiToolsList.reduce<MrkdwnOption[]>((acc, tool) => {
+    if (!preEnabledToolsSet.has(tool)) {
+      acc.push({
+        text: { type: 'mrkdwn', text: tool },
+        value: tool,
+      });
+    }
+    return acc;
+  }, []);
+
+  // Slack only allows 1 - 10 options per checkbox block, so we need to group the tools
+  let groupedTools = [];
+  for (let i = 0; i < formattedAndSanitizedApiToolsList.length; i += 10) {
+    const miniGroup = formattedAndSanitizedApiToolsList.slice(i, i + 10);
+    groupedTools.push(miniGroup);
+  }
+
+  const formatterGroupedTools = groupedTools.map((group, index) => {
+    return {
+      block_id: `tools_list_block_${index}`,
+      type: 'input',
+      optional: true,
+      element: {
+        type: 'checkboxes',
+        options: group,
+        action_id: 'checkbox_action',
+      },
+      label: {
+        type: 'plain_text',
+        text:
+          groupedTools.length > 1
+            ? `Available Tools  ${index + 1}/${groupedTools.length}`
+            : 'Available Tools',
+        emoji: true,
+      },
+    };
+  });
+
+  const blocks = [
+    formattedPreEnabledToolsList.length > 0 && {
+      block_id: 'enabled_tools_list_block',
+      type: 'input',
+      optional: true,
+      element: {
+        type: 'checkboxes',
+        options: formattedPreEnabledToolsList,
+        initial_options: formattedPreEnabledToolsList,
+        action_id: 'checkbox_action',
+      },
+      label: {
+        type: 'plain_text',
+        text: 'Enabled Tools',
+        emoji: true,
+      },
+    },
+    ...formatterGroupedTools,
+  ].filter((n) => n) as KnownBlock[];
+
+  await client.views
+    .open({
+      trigger_id: body.trigger_id,
+      view: {
+        private_metadata: JSON.stringify({
+          channelId,
+          numOfGroups: groupedTools.length,
+          hasPreEnabledTools: formattedPreEnabledToolsList.length > 0,
+        }),
+        callback_id: 'tools_view',
+        title: {
+          type: 'plain_text',
+          text: 'Tools Setup 🖇️',
+          emoji: true,
+        },
+        blocks,
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+          emoji: true,
+        },
+        type: 'modal',
+        close: {
+          type: 'plain_text',
+          text: 'Cancel',
+          emoji: true,
+        },
+      },
+    })
+    .catch(
+      async (error) =>
+        await handleError({
+          ...basicErrorHandlerArgs,
+          replyThreadTs: command.thread_ts,
+          customErrorMsg: error,
+        }),
+    );
+};
---
+++ src/interfaces/slack_bot/src/utils/getReply.ts
@@ -0,0 +1,86 @@
+import { AllMiddlewareArgs, AppMentionEvent, MessageEvent } from '@slack/bolt';
+
+import { ApiError, CohereChatRequest, OpenAPI, Tool, ToolkitClient } from '../cohere-client';
+import { ERRORS } from '../constants';
+import { formatRagCitations } from './formatRagCitations';
+import { getSanitizedMessage } from './getSanitizedMessage';
+import { GetUsersRealNameArgs, getUsersRealName } from './getUsersRealName';
+
+type GetReplyArgs = Pick<AllMiddlewareArgs, 'client'> & {
+  event: MessageEvent | AppMentionEvent;
+  tools?: Tool[];
+  model?: string | null;
+  temperature?: number | null;
+  preambleOverride?: string | null;
+  conversationId: string | undefined;
+  botUserId?: string;
+  isFirstMessage?: boolean;
+};
+
+type Reply = {
+  currentBotReply: string;
+  responseID: string;
+  errorMessage?: string;
+};
+
+/**
+ * Fetches a reply using the /chat endpoint given a
+ * slack event (that will contain the message text)
+ */
+
+export const getReply = async ({
+  event,
+  model,
+  client,
+  conversationId,
+  botUserId,
+  tools = [],
+  temperature = null,
+  preambleOverride = null,
+  isFirstMessage = false,
+}: GetReplyArgs): Promise<Reply> => {
+  /**
+   * Since we filter out edited messages in the message event listener,
+   * and the property we're looking for is present in both MessageEvent and AppMentionEvent,
+   * we can safely cast the event as AppMentionEvent
+   */
+  event = event as AppMentionEvent;
+  const messageWithoutUsername = await getSanitizedMessage({
+    message: event.text,
+    getUsersRealName: async ({ userId }: GetUsersRealNameArgs) =>
+      getUsersRealName({ userId, client }),
+    botUserId,
+    isFirstMessage,
+  });
+
+  const params: CohereChatRequest = {
+    message: messageWithoutUsername,
+    conversation_id: conversationId,
+    prompt_truncation: 'AUTO_PRESERVE_ORDER',
+  };
+
+  if (model && model !== 'default') params.model = model;
+  if (temperature) params.temperature = temperature;
+  if (preambleOverride) params.preamble = preambleOverride;
+  if (tools.length > 0) params.tools = tools;
+
+  try {
+    const toolkitClient = new ToolkitClient(OpenAPI);
+    const chatResponse = await toolkitClient.default.chatChatPost({
+      requestBody: params,
+    });
+    const sanitizedBotReply = await getSanitizedMessage({
+      message: chatResponse.text,
+      getUsersRealName: async ({ userId }: GetUsersRealNameArgs) =>
+        getUsersRealName({ userId, client }),
+    });
+
+    const botReply = sanitizedBotReply + formatRagCitations(chatResponse);
+    return { currentBotReply: botReply, responseID: chatResponse.generation_id || '' };
+  } catch (error: any) {
+    if (error instanceof ApiError) {
+      return { currentBotReply: '', responseID: '', errorMessage: error.message };
+    }
+    return { currentBotReply: '', responseID: '', errorMessage: ERRORS.GENERAL };
+  }
+};
