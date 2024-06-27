20bb9205846b4e7e70d37f97255d71348db7b382
Tianjing Li <tianjinglimail@gmail.com>
2024-06-27 13:49:16 -0400

    uncommit enabled auth

--- src/backend/config/auth.py
+++ src/backend/config/auth.py
@@ -2,7 +2,7 @@ from backend.services.auth import BasicAuthentication, GoogleOAuth, OpenIDConnec
 
 # Add Auth strategy classes here to enable them
 # Ex: [BasicAuthentication]
-ENABLED_AUTH_STRATEGIES = [OpenIDConnect]
+ENABLED_AUTH_STRATEGIES = []
 
 # Define the mapping from Auth strategy name to class obj - does not need to be manually modified.
 # During runtime, this will create an instance of each enabled strategy class.
