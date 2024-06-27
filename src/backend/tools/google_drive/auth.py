fb423e808a87f8ab9b2b712d0c602d83be15fb21
giannis <giannis@cohere.com>
2024-06-27 18:04:17 -0400

    p=t

--- src/backend/tools/google_drive/auth.py
+++ src/backend/tools/google_drive/auth.py
@@ -76,7 +76,7 @@ class GoogleDriveAuth(BaseToolAuthentication):
             raise ValueError("GOOGLE_DRIVE_CLIENT_ID not set")
         redirect_url = (
             os.getenv("NEXT_PUBLIC_API_HOSTNAME")
-            + "/v1/tool/auth?redirect_url=http://localhost:4000/new"
+            + "/v1/tool/auth?redirect_url=http://localhost:4000/new?p=t"
         )
         base_url = "https://accounts.google.com/o/oauth2/v2/auth?"
         state = {"user_id": user_id, "tool_id": GoogleDrive.NAME}
@@ -156,7 +156,7 @@ class GoogleDriveAuth(BaseToolAuthentication):
         state = json.loads(request.query_params.get("state"))
         redirect_url = (
             os.getenv("NEXT_PUBLIC_API_HOSTNAME")
-            + "/v1/tool/auth?redirect_url=http://localhost:4000/new"
+            + "/v1/tool/auth?redirect_url=http://localhost:4000/new?p=t"
         )
         url = "https://oauth2.googleapis.com/token"
         body = {
