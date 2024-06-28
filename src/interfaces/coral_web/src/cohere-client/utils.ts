5f56065426d59527519eb0caf4fc0992eccc6d4b
wujessica <jessica@cohere.ai>
2024-06-28 14:47:46 -0400

    Update chat description with latest message and fix finish reasons

--- src/interfaces/coral_web/src/cohere-client/utils.ts
+++ src/interfaces/coral_web/src/cohere-client/utils.ts
@@ -20,7 +20,24 @@ export const isFinishedError = (error: unknown): error is CohereFinishStreamErro
   error instanceof CohereFinishStreamError;
 
 export const isFinishedUnexpectedError = (error: unknown): error is CohereFinishStreamError =>
-  isFinishedError(error) && error.reason === FinishReason.FINISH_REASON_ERROR;
+  isFinishedError(error) && error.reason === FinishReason.ERROR;
 
 export const isStreamError = (error: unknown): error is CohereStreamError =>
   error instanceof CohereStreamError;
+
+export const getFinishReasonErrorMessage = (
+  reason: FinishReason | string | null | undefined,
+  error?: string | null
+): string => {
+  switch (reason) {
+    case FinishReason.ERROR: {
+      return error ?? 'An error occurred. Please try again.';
+    }
+    case FinishReason.MAX_TOKENS: {
+      return 'Generation stopped since max tokens limit was reached.';
+    }
+    default: {
+      return 'An error occurred. Please try again.';
+    }
+  }
+};
