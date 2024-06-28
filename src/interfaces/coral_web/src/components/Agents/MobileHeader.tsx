224af598ba8aa10e69ec9e62cdde94324f8b4527
wujessica <jessica@cohere.ai>
2024-06-28 15:10:09 -0400

    Add hamburger button on mobile

--- src/interfaces/coral_web/src/components/Agents/MobileHeader.tsx
+++ src/interfaces/coral_web/src/components/Agents/MobileHeader.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from '@/components/IconButton';
 import { Logo } from '@/components/Shared';
 import { useAgentsStore } from '@/stores';
 
@@ -12,10 +13,9 @@ export const MobileHeader: React.FC = () => {
   };
 
   return (
-    <header className="flex h-11 items-center justify-start rounded-lg border border-marble-400 bg-marble-200 px-4 lg:hidden">
-      <button onClick={onToggleAgentsSidePanel}>
-        <Logo />
-      </button>
+    <header className="flex h-11 items-center justify-start gap-x-1 rounded-lg border border-marble-400 bg-marble-200 px-5 lg:hidden">
+      <IconButton iconName="menu" onClick={onToggleAgentsSidePanel} />
+      <Logo />
     </header>
   );
 };
