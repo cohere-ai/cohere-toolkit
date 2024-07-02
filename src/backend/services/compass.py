b14159f92aa50a6986af7b0fd8457d5ce8bbb61c
giannis <giannis@cohere.com>
2024-07-02 17:32:08 -0400

    add compass index refresh

--- src/backend/services/compass.py
+++ src/backend/services/compass.py
@@ -30,6 +30,7 @@ class Compass:
         DELETE = "delete"
         GET_DOCUMENT = "get_document"
         ADD_CONTEXT = "add_context"
+        REFRESH = "refresh"
 
     def __init__(
         self,
@@ -55,9 +56,7 @@ class Compass:
             )
 
         self.compass_api_url = compass_api_url or os.getenv("COHERE_COMPASS_API_URL")
-        self.compass_parser_url = compass_parser_url or os.getenv(
-            "COHERE_COMPASS_PARSER_URL"
-        )
+        self.compass_parser_url = compass_parser_url or os.getenv("COHERE_COMPASS_PARSER_URL")
         self.username = compass_username or os.getenv("COHERE_COMPASS_USERNAME")
         self.password = compass_password or os.getenv("COHERE_COMPASS_PASSWORD")
         self.parser_config = parser_config
@@ -99,10 +98,7 @@ class Compass:
         """
 
         # Check if index is specified
-        if (
-            not parameters.get("index", None)
-            and action != self.ValidActions.LIST_INDEXES
-        ):
+        if not parameters.get("index", None) and action != self.ValidActions.LIST_INDEXES:
             raise Exception(
                 "Compass Tool: No index specified. ",
                 "No action will be taken. ",
@@ -129,6 +125,8 @@ class Compass:
                 return self._get_document(parameters, **kwargs)
             case self.ValidActions.ADD_CONTEXT:
                 self._add_context(parameters, **kwargs)
+            case self.ValidActions.REFRESH:
+                self._refresh(parameters, **kwargs)
             case _:
                 raise Exception(
                     f"Compass Tool: Invalid action {parameters['action']}. "
@@ -224,6 +222,10 @@ class Compass:
             context=parameters["context"],
         )
 
+    def _refresh(self, parameters: dict, **kwargs: Any) -> None:
+        """Refresh an index in Compass"""
+        self.compass_client.refresh(index_name=parameters["index"])
+
     def _process_file(self, parameters: dict, **kwargs: Any) -> None:
         """Parse the input file."""
         # Check if file_id is specified for file-related actions
@@ -235,9 +237,7 @@ class Compass:
             )
 
         # Check if filename is specified for file-related actions
-        if not parameters.get("filename", None) and not parameters.get(
-            "file_text", None
-        ):
+        if not parameters.get("filename", None) and not parameters.get("file_text", None):
             logger.error(
                 "Compass Tool: No filename or file_text specified for "
                 "create/update operation. "
@@ -259,9 +259,7 @@ class Compass:
             return None
 
         parser_config = self.parser_config or parameters.get("parser_config", None)
-        metadata_config = metadata_config = self.metadata_config or parameters.get(
-            "metadata_config", None
-        )
+        metadata_config = metadata_config = self.metadata_config or parameters.get("metadata_config", None)
 
         if filename:
             return self.parser_client.process_file(
@@ -289,9 +287,7 @@ class Compass:
             metadata_config=self.metadata_config,
             doc_id=file_id,
         )
-        auth = (
-            (self.username, self.password) if self.username and self.password else None
-        )
+        auth = (self.username, self.password) if self.username and self.password else None
         res = self.parser_client.session.post(
             url=f"{self.parser_client.parser_url}/v1/process_file",
             data={"data": json.dumps(params.model_dump())},
