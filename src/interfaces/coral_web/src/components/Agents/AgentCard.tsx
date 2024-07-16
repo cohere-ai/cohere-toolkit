ec9370d81d1080c20659ee263337664cfcf41a14
Khalil Najjar <knajjars@gmail.com>
2024-07-16 18:00:22 +0200

    fix(toolkit): active colors

--- src/interfaces/coral_web/src/components/Agents/AgentCard.tsx
+++ src/interfaces/coral_web/src/components/Agents/AgentCard.tsx
@@ -80,9 +80,9 @@ export const AgentCard: React.FC<Props> = ({ name, id, isBaseAgent, isExpanded }
       <div
         onClick={handleNewChat}
         className={cn(
-          'group flex w-full items-center justify-between gap-x-2 rounded-lg p-2 transition-colors hover:cursor-pointer hover:bg-marble-950',
+          'group flex w-full items-center justify-between gap-x-2 rounded-lg p-2 transition-colors hover:cursor-pointer hover:bg-mushroom-900/80',
           {
-            'bg-marble-950': isActive,
+            'bg-mushroom-900/80': isActive,
           }
         )}
       >
