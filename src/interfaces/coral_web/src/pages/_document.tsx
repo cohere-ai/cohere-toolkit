c0f62409cdae68f33373c08ae2a671b29f4ee21c
Khalil Najjar <knajjars@gmail.com>
2024-07-16 14:44:48 +0200

    format fix

--- src/interfaces/coral_web/src/pages/_document.tsx
+++ src/interfaces/coral_web/src/pages/_document.tsx
@@ -2,11 +2,8 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 import { env } from '@/env.mjs';
 import { cn } from '@/utils';
-import { useExperimentalFeatures } from '@/hooks/experimentalFeatures';
 
 export default function Document() {
-  // const { data: experimentalFeatures } = useExperimentalFeatures();
-  // console.debug(experimentalFeatures)
   return (
     <Html lang="en" className={cn({ dark: env.NEXT_PUBLIC_DARK_MODE })}>
       <Head />
