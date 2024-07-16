f7b04e0e20f887388898bed6f95fe82584500d4a
Tianjing Li <tianjinglimail@gmail.com>
2024-07-16 14:12:48 -0400

    Changes to FK to make alembic migration backwards compatible

--- src/backend/database_models/conversation.py
+++ src/backend/database_models/conversation.py
@@ -13,7 +13,9 @@ class Conversation(Base):
     __tablename__ = "conversations"
 
     id: Mapped[str] = mapped_column(String, default=lambda: str(uuid4()))
-    user_id: Mapped[str] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=True
+    )
     title: Mapped[str] = mapped_column(String, default="New Conversation")
     description: Mapped[str] = mapped_column(String, nullable=True, default=None)
 
