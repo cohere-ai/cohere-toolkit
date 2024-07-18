f2bb02b6eab0298af5bf142a8755ac5eec4ef083
lusmoura <luisa@cohere.com>
2024-07-18 11:40:08 +0100

    fix tests?

--- src/backend/config/auth.py
+++ src/backend/config/auth.py
@@ -12,9 +12,9 @@ load_dotenv()
 SKIP_AUTH = os.getenv("SKIP_AUTH", None)
 # Add Auth strategy classes here to enable them
 # Ex: [BasicAuthentication]
-ENABLED_AUTH_STRATEGIES = []
+ENABLED_AUTH_STRATEGIES = [OpenIDConnect]
 if "pytest" in sys.modules or SKIP_AUTH == "true":
-    ENABLED_AUTH_STRATEGIES = [OpenIDConnect]
+    ENABLED_AUTH_STRATEGIES = []
 
 # Define the mapping from Auth strategy name to class obj - does not need to be manually modified.
 # During runtime, this will create an instance of each enabled strategy class.
