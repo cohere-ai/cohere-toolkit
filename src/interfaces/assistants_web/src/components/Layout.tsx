f50a0777c9c2b68fb4431f0b2d8f3537dbe326b0
Tomeu Cabot <tomeu@cohere.com>
2024-07-23 17:42:51 +0200

    frontend(assistants): assisants page + card

--- src/interfaces/assistants_web/src/components/Layout.tsx
+++ src/interfaces/assistants_web/src/components/Layout.tsx
@@ -50,7 +50,7 @@ export const Layout: React.FC<Props> = ({ showSettingsDrawer = false, children }
             className={cn(
               'relative flex h-full min-w-0 flex-grow flex-col',
               'rounded-lg border',
-              'border-marble-950 bg-marble-1000',
+              'border-marble-950 bg-marble-1000 dark:border-volcanic-100 dark:bg-volcanic-100',
               'overflow-hidden'
             )}
           >
