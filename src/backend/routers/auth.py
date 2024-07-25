3254303be5bd23c59d25b25bea359f82fee08886
Scott Mountenay <scott@canadiense.net>
2024-07-25 09:38:38 -0600

    Add more logging to auth endpoints

--- src/backend/routers/auth.py
+++ src/backend/routers/auth.py
@@ -21,7 +21,7 @@ from backend.services.auth.utils import (
     get_or_create_user,
     is_enabled_authentication_strategy,
 )
-from backend.services.logger import get_logger
+from backend.services.logger import get_logger, send_log_message
 
 logger = get_logger()
 
@@ -84,7 +84,14 @@ async def login(request: Request, login: Login, session: DBSessionDep):
     strategy_name = login.strategy
     payload = login.payload
 
+    send_log_message(logger, "[Auth] Login request", "debug")
+
     if not is_enabled_authentication_strategy(strategy_name):
+        send_log_message(
+            logger,
+            f"[Auth] Invalid Authentication Strategy: {strategy_name}",
+            "debug",
+        )
         raise HTTPException(
             status_code=422, detail=f"Invalid Authentication strategy: {strategy_name}."
         )
@@ -94,6 +101,8 @@ async def login(request: Request, login: Login, session: DBSessionDep):
     strategy_payload = strategy.get_required_payload()
     if not set(strategy_payload).issubset(payload.keys()):
         missing_keys = [key for key in strategy_payload if key not in payload.keys()]
+        send_log_message(
+            logger, f"Missing the following keys in the payload: {missing_keys}.", "debug")
         raise HTTPException(
             status_code=422,
             detail=f"Missing the following keys in the payload: {missing_keys}.",
@@ -101,6 +110,7 @@ async def login(request: Request, login: Login, session: DBSessionDep):
 
     user = strategy.login(session, payload)
     if not user:
+        send_log_message(logger, f"Error logging user in: Strategy {strategy_name}", "debug")
         raise HTTPException(
             status_code=401,
             detail=f"Error performing {strategy_name} authentication with payload: {payload}.",
@@ -130,6 +140,7 @@ async def authorize(
         HTTPException: If authentication fails, or strategy is invalid.
     """
     if not code:
+        send_log_message(logger, "[Auth] Error authorizing login: No code provided", "debug")
         raise HTTPException(
             status_code=400,
             detail=f"Error calling /auth with invalid code query parameter.",
@@ -141,12 +152,14 @@ async def authorize(
             strategy_name = enabled_strategy_name
 
     if not strategy_name:
+        send_log_message(logger, "[Auth] Error authorizing login: Invalid strategy", "debug")
         raise HTTPException(
             status_code=400,
             detail=f"Error calling /auth with invalid strategy name: {strategy_name}.",
         )
 
     if not is_enabled_authentication_strategy(strategy_name):
+        send_log_message(logger, f"[Auth] Error authorizing login: Strategy {strategy_name} not enabled")
         raise HTTPException(
             status_code=404, detail=f"Invalid Authentication strategy: {strategy_name}."
         )
@@ -162,6 +175,7 @@ async def authorize(
         )
 
     if not userinfo:
+        send_log_message(logger, f"[Auth] Error authorizing login: Invalid token {token}", "debug")
         raise HTTPException(
             status_code=401, detail=f"Could not get user from auth token: {token}."
         )
