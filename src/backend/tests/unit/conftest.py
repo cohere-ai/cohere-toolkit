6dd6293610171bf49aa2b15b8b1184e065d8acdf
Alex W <alex@blinkenlights.ca>
2024-07-30 16:32:14 -0400

    Move existing backend unit tests to new tests/unit directory

--- src/backend/tests/unit/conftest.py
+++ src/backend/tests/unit/conftest.py
@@ -14,7 +14,7 @@ from backend.database_models import get_session
 from backend.main import app, create_app
 from backend.schemas.deployment import Deployment
 from backend.schemas.user import User
-from backend.tests.factories import get_factory
+from backend.tests.unit.factories import get_factory
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
