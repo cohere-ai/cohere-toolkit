53f2af57a7014b2926551a12c7522a02b83c3ac4
mtanzim <mtanzim@gmail.com>
2024-07-31 13:49:10 -0400

    fix flaky test

--- src/backend/tests/routers/test_agent.py
+++ src/backend/tests/routers/test_agent.py
@@ -736,7 +736,6 @@ def test_update_agent_with_tool_metadata_and_new_tool_metadata(
     )
 
     assert response.status_code == 200
-    updated_agent = response.json()
 
     tool_metadata = (
         session.query(AgentToolMetadata)
@@ -744,12 +743,19 @@ def test_update_agent_with_tool_metadata_and_new_tool_metadata(
         .all()
     )
     assert len(tool_metadata) == 2
-    assert tool_metadata[0].tool_name == "google_drive"
-    assert tool_metadata[0].artifacts == [
+    drive_tool = None
+    search_tool = None
+    for tool in tool_metadata:
+        if tool.tool_name == "google_drive":
+            drive_tool = tool
+        if tool.tool_name == "search_file":
+            search_tool = tool
+    assert drive_tool.tool_name == "google_drive"
+    assert drive_tool.artifacts == [
         {"url": "test", "name": "test", "type": "folder"}
     ]
-    assert tool_metadata[1].tool_name == "search_file"
-    assert tool_metadata[1].artifacts == [
+    assert search_tool.tool_name == "search_file"
+    assert search_tool.artifacts == [
         {"url": "test", "name": "test", "type": "file"}
     ]
 
