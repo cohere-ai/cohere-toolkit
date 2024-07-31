ef70cc3528dd74f499572bd6895af2183ef73e7a
Scott <scottchu@cohere.com>
2024-07-31 09:49:44 -0400

    fix configs

--- src/backend/database_models/conversation.py
+++ src/backend/database_models/conversation.py
@@ -2,28 +2,11 @@ from typing import List, Optional
 from uuid import uuid4
 
 from sqlalchemy import ForeignKey, Index, PrimaryKeyConstraint, String, UniqueConstraint
-from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.database_models.base import Base
+from backend.database_models.file import File
 from backend.database_models.message import Message
-
-
-class ConversationFileAssociation(Base):
-    __tablename__ = "conversation_files"
-
-    conversation_id: Mapped[str] = mapped_column(
-        ForeignKey("conversations.id", ondelete="CASCADE")
-    )
-    user_id: Mapped[str] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    file_id: Mapped[str] = mapped_column(String, default=None, nullable=False)
-    conversation: Mapped["Conversation"] = relationship(
-        "Conversation", back_populates="conversation_file_associations"
-    )
-
-    __table_args__ = (
-        UniqueConstraint("conversation_id", "file_id", name="unique_conversation_file"),
-    )
 
 
 class Conversation(Base):
@@ -33,9 +16,7 @@ class Conversation(Base):
     user_id: Mapped[str] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
     title: Mapped[str] = mapped_column(String, default="New Conversation")
     description: Mapped[str] = mapped_column(String, nullable=True, default=None)
-    conversation_file_associations: Mapped[List["ConversationFileAssociation"]] = (
-        relationship("ConversationFileAssociation", back_populates="conversation")
-    )
+
     text_messages: Mapped[List[Message]] = relationship()
     files: Mapped[List[File]] = relationship()
     agent_id: Mapped[str] = mapped_column(
@@ -52,13 +33,6 @@ class Conversation(Base):
     @property
     def messages(self):
         return sorted(self.text_messages, key=lambda x: x.created_at)
-
-    @property
-    def file_ids(self):
-        return [
-            conversation_file_association.file_id
-            for conversation_file_association in self.conversation_file_associations
-        ]
 
     __table_args__ = (
         UniqueConstraint("id", "user_id", name="conversation_id_user_id"),
