75da17ed2471e77719017fbcf93bed5dac728276
giannis <giannis@cohere.com>
2024-08-01 11:02:10 +0300

    make lint

--- src/backend/tools/google_drive/activity.py
+++ src/backend/tools/google_drive/activity.py
@@ -26,8 +26,13 @@ from backend.tools.google_drive.utils import get_current_timestamp_in_ms, get_se
 def handle_google_drive_activity_event(
     event_type: str, activity: Dict[str, str], agent_id: str, user_id: str, **kwargs
 ):
-    index_name = "{}_{}".format(agent_id if agent_id is not None else user_id, GoogleDrive.NAME)
-    (file_ids, titles) = (_extract_file_ids_from_target(activity=activity)[key] for key in ("file_ids", "titles"))
+    index_name = "{}_{}".format(
+        agent_id if agent_id is not None else user_id, GoogleDrive.NAME
+    )
+    (file_ids, titles) = (
+        _extract_file_ids_from_target(activity=activity)[key]
+        for key in ("file_ids", "titles")
+    )
     if not file_ids:
         return
 
@@ -99,7 +104,9 @@ def handle_google_drive_activity_event(
             raise Exception("This action is not tracked for Google Drive")
 
 
-def query_google_drive_activity(session: Session, agent: Agent, agent_artifacts: List[Dict[str, str]]):
+def query_google_drive_activity(
+    session: Session, agent: Agent, agent_artifacts: List[Dict[str, str]]
+):
     user_id = agent.user_id
     gdrive_auth = GoogleDriveAuth()
     agent_creator_auth_token = gdrive_auth.get_token(session=session, user_id=user_id)
@@ -107,15 +114,23 @@ def query_google_drive_activity(session: Session, agent: Agent, agent_artifacts:
         raise Exception("Sync GDrive Error: No agent creator credentials found")
 
     if gdrive_auth.is_auth_required(session, user_id=user_id):
-        raise Exception("Sync GDrive Error: Agent creator credentials need to re-authenticate")
+        raise Exception(
+            "Sync GDrive Error: Agent creator credentials need to re-authenticate"
+        )
 
-    (service,) = (get_service(api="driveactivity", version="v2", user_id=user_id)[key] for key in ("service",))
+    (service,) = (
+        get_service(api="driveactivity", version="v2", user_id=user_id)[key]
+        for key in ("service",)
+    )
 
-    activity_ts_filter = get_current_timestamp_in_ms(negative_offset=ACTIVITY_TRACKING_WINDOW)
+    activity_ts_filter = get_current_timestamp_in_ms(
+        negative_offset=ACTIVITY_TRACKING_WINDOW
+    )
     activities = []
     with futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures_list = [
-            executor.submit(_get_activity, service, artifact, activity_ts_filter) for artifact in agent_artifacts
+            executor.submit(_get_activity, service, artifact, activity_ts_filter)
+            for artifact in agent_artifacts
         ]
         for future in futures.as_completed(futures_list):
             try:
@@ -123,7 +138,10 @@ def query_google_drive_activity(session: Session, agent: Agent, agent_artifacts:
             except Exception as e:
                 raise e
 
-    return {agent_artifacts[index]["id"]: activities[index] for index in range(len(activities))}
+    return {
+        agent_artifacts[index]["id"]: activities[index]
+        for index in range(len(activities))
+    }
 
 
 def _get_activity(
@@ -142,8 +160,16 @@ def _get_activity(
                     activity_ts_filter,
                     " ".join([e.value.upper() for e in GoogleDriveActions]),
                 ),
-                **({"ancestorName": "items/{}".format(artifact_id)} if artifact_type == "folder" else {}),
-                **({"itemName": "items/{}".format(artifact_id)} if artifact_type == "file" else {}),
+                **(
+                    {"ancestorName": "items/{}".format(artifact_id)}
+                    if artifact_type == "folder"
+                    else {}
+                ),
+                **(
+                    {"itemName": "items/{}".format(artifact_id)}
+                    if artifact_type == "file"
+                    else {}
+                ),
                 "pageToken": next_page_token,
                 "consolidationStrategy": {
                     "legacy": {},
