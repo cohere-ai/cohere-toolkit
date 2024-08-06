944daaeedab1d034391d2b34b17803953f2669bb
Khalil Najjar <knajjars@gmail.com>
2024-08-06 14:27:40 +0200

    feat(toolkit): preload data in RSC

--- src/interfaces/assistants_web/src/app/(main)/(chat)/a/[agentId]/page.tsx
+++ src/interfaces/assistants_web/src/app/(main)/(chat)/a/[agentId]/page.tsx
@@ -1,6 +1,8 @@
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { NextPage } from 'next';
 
 import Chat from '@/app/(main)/(chat)/Chat';
+import { getCohereServerClient } from '@/server/cohereServerClient';
 
 type Props = {
   params: {
@@ -9,8 +11,20 @@ type Props = {
   searchParams: Record<string, string>;
 };
 
-const Page: NextPage<Props> = ({ params }) => {
-  return <Chat agentId={params.agentId} />;
+const Page: NextPage<Props> = async ({ params }) => {
+  const cohereServerClient = getCohereServerClient();
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['agent', params.agentId],
+    queryFn: async () => cohereServerClient.getAgent(params.agentId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat agentId={params.agentId} />
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
