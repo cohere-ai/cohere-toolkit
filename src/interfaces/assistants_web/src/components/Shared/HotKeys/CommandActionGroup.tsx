4c86c4baffe52ca1088d1f0320c7a35e10188a97
Tomeu Cabot <tomeu@cohere.com>
2024-08-08 14:02:16 +0200

    refactor: hotkeys supports group name

---
+++ src/interfaces/assistants_web/src/components/Shared/HotKeys/CommandActionGroup.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Text } from '@/components/Shared';
+import { CommandAction } from '@/components/Shared/HotKeys/CommandAction';
+import { type HotKeyGroupOption } from '@/components/Shared/HotKeys/domain';
+
+type Props = {
+  isOpen: boolean;
+  options?: HotKeyGroupOption[];
+};
+
+const CommandActionGroup: React.FC<Props> = ({ isOpen, options = [] }) => {
+  return options.map((action) => {
+    if (action.quickActions.length === 0) return null;
+    return (
+      <section key={action.group}>
+        <Text styleAs="p-sm" className="mx-3 p-4 uppercase dark:text-marble-800">
+          {action.group}
+        </Text>
+        {action.quickActions.map((quickAction) => (
+          <CommandAction key={quickAction.name} isOpen={isOpen} {...quickAction} />
+        ))}
+      </section>
+    );
+  });
+};
+
+export default CommandActionGroup;
