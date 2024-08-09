eb5e7ae29c5fc75e660ffbcab8a577fddefe1abd
Raphael Cristal <raphael@cohere.com>
2024-08-09 11:46:44 +0200

    can not be null

--- src/backend/database_models/group.py
+++ src/backend/database_models/group.py
@@ -7,7 +7,7 @@ from backend.database_models.base import Base
 class Group(Base):
     __tablename__ = "groups"
 
-    display_name: Mapped[str] = mapped_column(String, nullable=True)
+    display_name: Mapped[str] = mapped_column(String)
 
     users = relationship("User", secondary="user_group", backref="groups")
     user_associations = relationship("UserGroupAssociation", back_populates="group")
