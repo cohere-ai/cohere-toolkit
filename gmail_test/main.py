c6e0e746a712b54145b91faddf7c851c67a9402a
mtanzim <mtanzim@gmail.com>
2024-08-13 12:18:31 -0400

    gmail tester code

--- gmail_test/main.py
+++ gmail_test/main.py
@@ -28,7 +28,7 @@ def main():
       flow = InstalledAppFlow.from_client_secrets_file(
           "credentials.json", SCOPES
       )
-      creds = flow.run_local_server(port=0)
+      creds = flow.run_local_server(port=53091)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
