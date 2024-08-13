6803a8d7ece9d478a6c2a245da6a5762ed4866b8
Khalil Najjar <knajjars@gmail.com>
2024-08-13 16:48:35 +0200

    add query hydration

--- src/interfaces/assistants_web/src/app/(main)/(chat)/c/[conversationId]/page.tsx
+++ src/interfaces/assistants_web/src/app/(main)/(chat)/c/[conversationId]/page.tsx
@@ -2,6 +2,7 @@ import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query
 import { NextPage } from 'next';
 
 import Chat from '@/app/(main)/(chat)/Chat';
+import { BASE_AGENT } from '@/constants';
 import { getCohereServerClient } from '@/server/cohereServerClient';
 
 type Props = {
@@ -20,6 +21,10 @@ const Page: NextPage<Props> = async ({ params }) => {
       queryKey: ['conversation', params.conversationId],
       queryFn: async () =>
         cohereServerClient.getConversation({ conversationId: params.conversationId }),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ['agent', null],
+      queryFn: () => BASE_AGENT,
     }),
   ]);
 
