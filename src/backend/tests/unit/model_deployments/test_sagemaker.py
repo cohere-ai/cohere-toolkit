b89f1e373860e9a348c6cd6ca7dfebc7cf0cb1ed
Michael Kozakov <michael.kozakov@gmail.com>
2024-08-16 02:42:02 -0400

    introduce import error

--- src/backend/tests/unit/model_deployments/test_sagemaker.py
+++ src/backend/tests/unit/model_deployments/test_sagemaker.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from backend.config.deployments import ModelDeploymentName
+
 from backend.database_models.user import User
 from backend.tests.unit.model_deployments.mock_deployments import (
     MockSageMakerDeployment,
