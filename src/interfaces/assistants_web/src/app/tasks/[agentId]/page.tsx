1f33d2e5bc739355aacbadc23c5d8c3741fabe1a
mtanzim <mtanzim@gmail.com>
2024-08-16 16:23:24 -0400

    thanks tailwind

--- src/interfaces/assistants_web/src/app/tasks/[agentId]/page.tsx
+++ src/interfaces/assistants_web/src/app/tasks/[agentId]/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { NextPage } from 'next';
-
-import { useAgentTasks } from '@/hooks/agents';
+import { useAgent, useAgentTasks } from '@/hooks/agents';
 
 type Props = {
   params: {
@@ -11,13 +9,48 @@ type Props = {
   searchParams: Record<string, string>;
 };
 
-const Page: NextPage<Props> = ({ params }) => {
+const Page = ({ params }: Props) => {
   const agentId = params.agentId;
-  const { data: agentTasks } = useAgentTasks({ agentId });
+  const {
+    data: agentTasks,
+    isLoading: isTaskLoading,
+    error: isTaskError,
+  } = useAgentTasks({ agentId });
+  const { data: agent, isLoading, error } = useAgent({ agentId });
+
+  if (error || isTaskError) {
+    return <div>Error: {error?.message}</div>;
+  }
+
+  if (isLoading || isTaskLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div>
-      <h1>Tasks will be here for {params.agentId}</h1>;<code>{JSON.stringify(agentTasks)}</code>
+    <div className="m-12">
+      <h1 className="mb-4 mt-4 text-2xl bg-slate-400 p-16">Tasks report for {agent?.name}</h1>
+      <table className="table-fixed bg-slate-400 p-16 text-balance border-separate border-spacing-2 border border-slate-500 ">
+        <thead className="text-xl">
+          <tr>
+            <th>Task ID</th>
+            <th>Task Name</th>
+            <th>Task Status</th>
+            <th>Task Result</th>
+            <th>Exception Snippet</th>
+          </tr>
+        </thead>
+        <tbody className="text">
+          {(agentTasks || []).map((task) => (
+            <tr key={task.task_id}>
+              <td>{task.task_id}</td>
+              <td>{task.name}</td>
+              <td>{task.status}</td>
+              <td>{JSON.stringify(task.result, null, 2)}</td>
+              <td>{task?.exception_snippet || ''}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
