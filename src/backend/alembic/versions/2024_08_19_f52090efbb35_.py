b3b3dd2c60a598288ade7579eefd999f0646aad5
mtanzim <mtanzim@gmail.com>
2024-08-19 13:35:27 -0400

    fix lint

--- src/backend/alembic/versions/2024_08_19_f52090efbb35_.py
+++ src/backend/alembic/versions/2024_08_19_f52090efbb35_.py
@@ -7,10 +7,6 @@ Create Date: 2024-08-19 17:30:35.031698
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'f52090efbb35'
 down_revision: Union[str, None] = ('0091b3b3b4dc', 'c301506b3676')
