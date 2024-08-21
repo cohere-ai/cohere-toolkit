b06fe64ead75355d7d5034b37e084ee6d6e0529e
Scott <scottchu@cohere.com>
2024-08-21 17:49:35 -0400

    lint

--- src/backend/alembic/versions/2024_08_21_5d63f6cf2360_.py
+++ src/backend/alembic/versions/2024_08_21_5d63f6cf2360_.py
@@ -7,9 +7,8 @@ Create Date: 2024-08-21 21:45:10.616983
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '5d63f6cf2360'
