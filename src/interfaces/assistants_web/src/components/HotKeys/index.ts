c1ff137a115ee125e844622899e84843cd99f07b
Abigail Mackenzie-Armes <abigail@cohere.com>
2024-08-21 15:57:14 -0400

    fix

--- src/interfaces/assistants_web/src/components/HotKeys/index.ts
+++ src/interfaces/assistants_web/src/components/HotKeys/index.ts
@@ -3,4 +3,5 @@ export * from './CommandActionGroup';
 export * from './domain';
 export * from './HotKeys';
 export * from './HotKeysDialog';
+export * from './HotKeysDialogInput';
 export * from './HotKeysProvider';
