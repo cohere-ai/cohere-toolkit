466166a05b5c38c22400bf63f37bb3f1393bcc9d
Danylo Boiko <danielboyko02@gmail.com>
2024-09-04 02:27:19 +0300

    Fix accept attribute for inputs

--- src/interfaces/assistants_web/src/utils/file.ts
+++ src/interfaces/assistants_web/src/utils/file.ts
@@ -72,3 +72,30 @@ export const mapExtensionToMimeType = (extension: string) => {
     }[extension] || 'text/plain'
   );
 };
+
+/**
+ * Map mime type to file extension.
+ * @param mimeType
+ * @returns extension
+ */
+export const mapMimeTypeToExtension = (mimeType: string) => {
+  return (
+    {
+      ['text/csv']: 'csv',
+      ['text/tab-separated-values']: 'tsv',
+      ['text/html']: 'html',
+      ['text/markdown']: 'md',
+      ['application/msword']: 'doc',
+      ['application/vnd.openxmlformats-officedocument.wordprocessingml.document']: 'docx',
+      ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']: 'xlsx',
+      ['application/vnd.openxmlformats-officedocument.presentationml.presentation']: 'pptx',
+      ['application/json']: 'json',
+      ['application/pdf']: 'pdf',
+      ['application/epub+zip']: 'epub',
+      ['application/vnd.apache.parquet']: 'parquet',
+      ['image/png']: 'png',
+      ['text/plain']: 'txt',
+      ['application/zip']: 'zip',
+    }[mimeType] || 'txt'
+  );
+};
