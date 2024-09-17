07ce1e537773f4a21054cca61648ffdfc45eb2e3
Abigail MacKenzie-Armes <70247056+abimacarmes@users.noreply.github.com>
2024-09-17 05:48:50 -0400

    fix(toolkit): fix download file (#748)
    fix download file

--- src/interfaces/assistants_web/src/components/Markdown/tags/Anchor.tsx
+++ src/interfaces/assistants_web/src/components/Markdown/tags/Anchor.tsx
@@ -6,19 +6,21 @@ import { ComponentPropsWithoutRef } from 'react';
 import { Icon } from '@/components/UI';
 import { useOutputFiles } from '@/stores';
 
-export const Anchor: Component<ComponentPropsWithoutRef<'a'> & ExtraProps> = ({ children }) => {
+export const Anchor: Component<ComponentPropsWithoutRef<'a'> & ExtraProps> = ({
+  children,
+  node,
+}) => {
   const { outputFiles } = useOutputFiles();
+  const nodeHref = node?.properties.href;
 
-  if (typeof children === 'string') {
-    const snakeCaseUrl = children.replace(/\s/g, '_').toLowerCase();
-    const outputFile = Object.entries(outputFiles).find(([key]) =>
-      key.startsWith(snakeCaseUrl)
-    )?.[1];
+  if (typeof children === 'string' && typeof nodeHref === 'string') {
+    const outputFile = Object.entries(outputFiles).find(([key]) => nodeHref.includes(key))?.[1];
+
     const downloadUrl = outputFile?.downloadUrl;
 
     if (downloadUrl) {
       return (
-        <a href={downloadUrl} download={snakeCaseUrl} className="flex items-center gap-1">
+        <a href={downloadUrl} download={outputFile.name} className="flex items-center gap-1">
           {children}
           <Icon name="download" />
         </a>
