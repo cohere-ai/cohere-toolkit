8eaccd9b54c75366f5d921f87137de5bbb97552b
Tianjing Li <tianjinglimail@gmail.com>
2024-11-26 11:48:38 -0500

    Mock Google Drive for CI

--- src/backend/tests/unit/routers/test_tool.py
+++ src/backend/tests/unit/routers/test_tool.py
@@ -1,11 +1,15 @@
 from unittest.mock import patch
 
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 from backend.config.tools import Tool, get_available_tools
+from backend.schemas.tool import ToolCategory, ToolDefinition
 from backend.schemas.user import User
 from backend.tests.unit.factories import get_factory
+from backend.tools.base import BaseTool
+from backend.tools.google_drive.auth import GoogleDriveAuth
 
 TOOL_DEFINITION_KEYS = [
     "name",
@@ -18,7 +22,12 @@ TOOL_DEFINITION_KEYS = [
     "description"
 ]
 
-def test_list_tools(session_client: TestClient, session: Session) -> None:
+@pytest.fixture
+def mock_get_available_tools():
+    with patch("backend.routers.tool.get_available_tools") as mock:
+        yield mock
+
+def test_list_tools(session_client: TestClient) -> None:
     response = session_client.get("/v1/tools")
     assert response.status_code == 200
     available_tools = get_available_tools()
@@ -29,7 +38,33 @@ def test_list_tools(session_client: TestClient, session: Session) -> None:
         for key in TOOL_DEFINITION_KEYS:
             assert tool[key] == getattr(tool_definition, key)
 
-def test_list_authed_tool_should_return_token(session_client: TestClient, session: Session) -> None:
+def test_list_authed_tool_should_return_token(session_client: TestClient, mock_get_available_tools) -> None:
+    class MockGoogleDrive(BaseTool):
+        ID = "google_drive"
+        @classmethod
+        def get_tool_definition(cls) -> ToolDefinition:
+            return ToolDefinition(
+                name=cls.ID,
+                display_name="Google Drive",
+                implementation=cls,
+                parameter_definitions={
+                    "query": {
+                        "description": "Query to search Google Drive documents with.",
+                        "type": "str",
+                        "required": True,
+                    }
+                },
+                is_visible=True,
+                is_available=True,
+                auth_implementation=GoogleDriveAuth,
+                should_return_token=True,
+                error_message=cls.generate_error_message(),
+                category=ToolCategory.DataLoader,
+                description="Returns a list of relevant document snippets from the user's Google drive.",
+            )
+
+    # Patch Google Drive tool
+    mock_get_available_tools.return_value = {Tool.Google_Drive.value.ID: MockGoogleDrive.get_tool_definition()}
     # Patch get_token method for Google Drive
     with patch('backend.tools.google_drive.tool.GoogleDriveAuth.get_token', return_value="Mocked Value"):
         response = session_client.get("/v1/tools")
@@ -40,7 +75,7 @@ def test_list_authed_tool_should_return_token(session_client: TestClient, sessio
         if tool["should_return_token"]:
             assert tool["token"] == "Mocked Value"
 
-def test_list_authed_tool_should_not_return_token(session_client: TestClient, session: Session) -> None:
+def test_list_authed_tool_should_not_return_token(session_client: TestClient) -> None:
     response = session_client.get("/v1/tools")
 
     assert response.status_code == 200
@@ -49,8 +84,8 @@ def test_list_authed_tool_should_not_return_token(session_client: TestClient, se
         if not tool["should_return_token"]:
             assert tool["token"] == ""
 
-def test_list_tools_error_message_none_if_available(client: TestClient) -> None:
-    response = client.get("/v1/tools")
+def test_list_tools_error_message_none_if_available(session_client: TestClient) -> None:
+    response = session_client.get("/v1/tools")
     assert response.status_code == 200
     for tool in response.json():
         if tool["is_available"]:
@@ -77,7 +112,7 @@ def test_list_tools_with_agent(
         assert tool[key] == getattr(tool_definition, key)
 
 def test_list_tools_with_agent_that_doesnt_exist(
-    session_client: TestClient, session: Session
+    session_client: TestClient
 ) -> None:
     response = session_client.get("/v1/tools", params={"agent_id": "fake_id"})
     assert response.status_code == 404
