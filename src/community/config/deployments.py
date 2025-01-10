af02717ad1d5b82d1e706322855d0dbc03781af5
Tianjing Li <tianjinglimail@gmail.com>
2025-01-10 12:37:41 -0500

    Minor changes

--- src/community/config/deployments.py
+++ src/community/config/deployments.py
@@ -1,14 +1,6 @@
-from enum import StrEnum
-
 from community.model_deployments.community_deployment import CommunityDeployment
 
 # Add the below for local model deployments
 # from community.model_deployments.local_model import LocalModelDeployment
-
-
-class ModelDeploymentName(StrEnum):
-    HuggingFace = "HuggingFace"
-    LocalModel = "LocalModel"
-
 
 AVAILABLE_MODEL_DEPLOYMENTS = { d.name(): d for d in CommunityDeployment.__subclasses__() }
