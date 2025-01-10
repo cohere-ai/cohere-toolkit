86cb4d93f1aa661b82345a83877e4875e4fa591c
Eric Zawadski <ericz@live.ca>
2025-01-10 11:28:09 -0800

    feat(backend): added verbose title and description to deployment params and schemas

---
+++ src/backend/schemas/params/deployment.py
@@ -0,0 +1,21 @@
+"""
+Query and Path Parameters for Deployments
+"""
+from typing import Annotated, Optional
+
+from fastapi import Path, Query
+
+DeploymentIdPathParam = Annotated[str, Path(
+    title="Deployment ID",
+    description="Deployment ID for deployment in question",
+)]
+
+DeploymentNamePathParam = Annotated[str, Path(
+    title="Deployment Name",
+    description="Deployment Name for deployment in question",
+)]
+
+AllQueryParam = Annotated[Optional[bool], Query(
+    title="All",
+    description="Include all deployments, regardless of availability.",
+)]
