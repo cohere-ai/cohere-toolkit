7eff4729c4fcac3f976bf190224b13cfe900ff00
EugeneP <eugene@lightsonsoftware.com>
2025-01-24 23:00:30 +0100

    RELEASE v.1.1.6 - FE clients

--- src/interfaces/coral_web/src/cohere-client/generated/core/OpenAPI.ts
+++ src/interfaces/coral_web/src/cohere-client/generated/core/OpenAPI.ts
@@ -47,7 +47,7 @@ export const OpenAPI: OpenAPIConfig = {
   PASSWORD: undefined,
   TOKEN: undefined,
   USERNAME: undefined,
-  VERSION: '1.1.5',
+  VERSION: '1.1.6',
   WITH_CREDENTIALS: false,
   interceptors: {
     request: new Interceptors(),
